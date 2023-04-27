b64d4af422afc811893fcb99aacee99e92e3b404
Sava Lakicevic <savva.lakichevich@gmail.com>
2023-04-27 11:21:13 +0200

    Added global and about section style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+// Import Global Styles
+import GlobalStyle from "./components/GlobalStyle";
+// Import Pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
     return (
         <div className="App">
+            <GlobalStyle></GlobalStyle>
             <AboutUs></AboutUs>
         </div>
     );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,23 @@
 import React from "react";
 import home1 from "../images/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <StyledAbout>
+            <StyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide>
                         <h2>We work to make </h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>
                             your <span>dreams</span> come
                         </h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>true</h2>
-                    </div>
+                    </StyledHide>
                 </div>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -24,12 +25,41 @@ const AboutSection = () => {
                     voluptatibus aliquid, quidem reiciendis!
                 </p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </StyledDescription>
+            <StyledImage>
                 <img src={home1} alt="A person with a professinoal camera" />
-            </div>
-        </div>
+            </StyledImage>
+        </StyledAbout>
     );
 };
+
+// Styled Components
+const StyledAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+const StyledDescription = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+        font-weight: lighter;
+    }
+`;
+const StyledImage = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img {
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
+const StyledHide = styled.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
